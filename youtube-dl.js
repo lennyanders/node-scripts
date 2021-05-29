fac00f0800947fca0c8e7351513258f213c1b99a
5onderling <lenny.anders.98@gmail.com>
2021-05-29 23:37:28 +0200

    make youtube downloader accept youtube video id

--- youtube-dl.js
+++ youtube-dl.js
@@ -3,19 +3,19 @@
 import ytDl from 'youtube-dl-exec';
 import ffmpegLocation from 'ffmpeg-static';
 
-let [url] = process.argv.slice(2);
+let [urlOrYtId] = process.argv.slice(2);
 
-if (!url) {
+if (!urlOrYtId) {
   console.log('please provide an URL for the download');
   process.exit();
 }
 
-if (!/^http.?:\/\//.test(url)) url = `https://${url}`;
+if (/\..+/.test(urlOrYtId) && !/^http.?:\/\//.test(urlOrYtId)) urlOrYtId = `https://${urlOrYtId}`;
 
 (async () => {
   try {
     await ytDl(
-      url,
+      urlOrYtId,
       { format: 'bestvideo+bestaudio/best', output: '%(title)s.%(ext)s', ffmpegLocation },
       { stdout: 'inherit' },
     );
